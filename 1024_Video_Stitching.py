ac7004afdb000e0e0ea737ddb281b500f31b9ffd
Divyansh Agarwal <57028983+divyansh7877@users.noreply.github.com>
2024-03-01 14:02:37 -0500

    Added third problem

---
+++ 1024_Video_Stitching.py
@@ -0,0 +1,26 @@
+class Solution:
+    def videoStitching(self, clips: list[list[int]], time: int) -> int:
+        clips.sort()
+        clips.append((time,time))
+        max_end=-1
+        pivot_start=0
+        cnt=0
+
+        i=0
+        while i<len(clips):
+            start,end=clips[i]
+
+            if start<=pivot_start:
+                max_end=max(max_end,end)
+                i+=1
+
+            else:
+                pivot_start=max_end
+                cnt+=1
+                if max_end>=time:
+                    return cnt
+
+                elif start>max_end:
+                    return -1
+
+
