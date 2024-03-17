e369d73049518468397d8aed03356667a29a179f
Divyansh Agarwal <57028983+divyansh7877@users.noreply.github.com>
2024-03-17 18:06:05 -0400

    Week 3, problem two.

---
+++ Week 3/36. Valid Sudoku.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        '''
+        We solve the sudoku by checking the neccessary rules
+            1. Check the elemnt with row index. 
+            2. Check the element with col index.
+            3. Check the subgrid.
+        
+        If the set length is same as original length, ie, we have no duplicates hence the Sudoku is valid.
+        '''
+        store=[]
+
+        for i in range(9):
+            for j in range(9):
+                ele=board[i][j]
+                if ele.isdigit():
+                    store+=[(i,ele),(ele,j),(i//3,j//3,ele)] # [row check,column check,3x3 grid check]
+
+        return len(store)==len(set(store)) # Check for possible duplicates.
